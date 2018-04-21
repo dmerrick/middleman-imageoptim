247aceef1ced36743cd01cce4e1a5bfbd1b8606c
Dana Merrick <dana@snapdocs.com>
2018-04-21 14:45:25 -0500

    Maybe this will work

--- lib/middleman-imageoptim/manifest.rb
+++ lib/middleman-imageoptim/manifest.rb
@@ -10,7 +10,7 @@ module Middleman
       end
 
       def path
-        File.join(build_dir, MANIFEST_FILENAME)
+        File.join(build_dir, '..', MANIFEST_FILENAME)
       end
 
       def build_dir
